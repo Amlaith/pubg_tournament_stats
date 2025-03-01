92bf805994d1e79b84d9060187d5ca00121f620d
Amlaith <burjooi@gmail.com>
2025-03-01 06:49:33 +0300

    first test

--- app.py
+++ app.py
@@ -6,41 +6,45 @@ with open('style.css') as f:
 def main():
     st.write("# GAZ CUP")
 
-    # Bottom-aligned columns
     col1, col2 = st.columns(2, gap='large')
 
     # You can also use "with" notation:
     with col1:
         st.subheader('Главное правило')
         st.markdown("""
-                    **Смурфинг или стримснайпинг — дисквалификация и бан на будущих турнирах**  
+                    **Смурфинг или стримснайпинг – дисквалификация и бан на будущих турнирах**  
                     *Решение о дисквалификации выносится по усмотрению организатора*
-                    """)
-                
-        st.subheader('Контакты')
-        st.markdown("""
-                    [https://t.me/gazcupPUBG](https://t.me/gazcupPUBG)
-                    [https://discord.gg/gSUJckTg](https://discord.gg/gSUJckTg)
-                    [https://www.twitch.tv/gazbro27](https://www.twitch.tv/gazbro27)
                     """)
 
     with col2:
         st.subheader('Призовой фонд')
         st.markdown("""
-                    - 1 место - 5 000 р.
-                    - 2 место - 3 000 р.
-                    - 3 место - 2 000 р.
+                    - 1 место – 5 000 р.
+                    - 2 место – 3 000 р.
+                    - 3 место – 2 000 р.
                     """)
-        
+            
+    col3, col4 = st.columns(2, gap='large')
+    with col3:
         st.subheader('Игровой процесс')
         st.markdown("""
                     Лобби создаются на европейском сервере в режиме "киберспорт".
 
                     Название лобби и пароль присылаются в группу капитанов непосредственно перед началом каждого матча.
 
-                    Время ожидания команд в лобби — 5 минут после оповещения капитанов.
+                    Время ожидания команд в лобби – 5 минут после оповещения капитанов.
                     """)
         
+    with col4:
+        st.subheader('Контакты')
+        st.markdown("""
+                    [Telegram](https://t.me/gazcupPUBG)  
+                    [Discord](https://discord.gg/gSUJckTg)  
+                    [Twitch](https://www.twitch.tv/gazbro27)  
+                    """)
+
+
+
     st.subheader('Подсчет баллов')
     st.markdown("""
                 Итоговые баллы = сумма баллов команды за все матчи турнира  
@@ -65,14 +69,20 @@ def main():
                 - Затем учитывается сумма баллов за последний матч  
                 - Затем учитывается место, занятое в последнем матче  
                 """)
+    st.subheader('Личный рейтинг')
+    st.markdown("""
+                5 игроков с наибольшим количеством киллов выигрывают бесплатное участие в следующем турнире  
+
+                В случае равного количества киллов у нескольких игроков, выбирается игрок с наименьшим уровнем аккаунта		
+                """)
     
     
 
 
-homepage = st.Page(main, title="Главная страница", icon=":material/home:")
-gazcup_1 = st.Page("tournaments_pages/3_GazCup_1.py", title="GAZ CUP #1", icon=":material/table:")
-gazcup_2a = st.Page("tournaments_pages/1_GazCup_2a.py", title="GAZ CUP #2, лобби A", icon=":material/trophy:")
-gazcup_2b = st.Page("tournaments_pages/2_GazCup_2b.py", title="GAZ CUP #2, лобби B", icon=":material/trophy:")
+homepage = st.Page(main, title="Главная страница", icon=":material/trophy:")
+gazcup_1 = st.Page("tournaments_pages/3_GazCup_1.py", title="GAZ CUP #1", icon=":material/history:")
+gazcup_2a = st.Page("tournaments_pages/1_GazCup_2a.py", title="GAZ CUP #2, лобби A", icon=":material/table:")
+gazcup_2b = st.Page("tournaments_pages/2_GazCup_2b.py", title="GAZ CUP #2, лобби B", icon=":material/table:")
 
 
 pg = st.navigation(
